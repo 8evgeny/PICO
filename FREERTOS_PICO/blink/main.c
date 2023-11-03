debbd6982aed43209857a81fe9a6b5f3452ef276
8evgeny <8evgeny@gmail.com>
2023-11-03 23:42:46 +0300

    6 задач в 6 потоках  работают

--- FREERTOS_PICO/blink/main.c
+++ FREERTOS_PICO/blink/main.c
@@ -6,6 +6,8 @@ typedef struct struct_arg_t {
      uint16_t period;
 } arg;
 void vBlinkTask(struct struct_arg_t * arg) {
+    gpio_init(arg->pin);
+    gpio_set_dir(arg->pin, GPIO_OUT);
    for (;;) {
       gpio_put(arg->pin, 1);
       vTaskDelay(arg->period);
@@ -14,22 +16,18 @@ void vBlinkTask(struct struct_arg_t * arg) {
    }
 }
 
-void main() {
+int main() {
 
-   gpio_init(PICO_DEFAULT_LED_PIN);
-   gpio_init(6);
-   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-   gpio_set_dir(6, GPIO_OUT);
-
-   arg arg1, arg2;
-   arg1.period = 300;
+   arg arg1, arg2, arg3, arg4, arg5, arg6;
+   arg1.period = 100;arg2.period = 200;arg3.period = 300;arg4.period = 400;arg5.period = 500;arg6.period = 600;
    arg1.pin = PICO_DEFAULT_LED_PIN;
-   arg2.period = 1000;
-   arg2.pin = 6;
+   arg2.pin = 2;arg3.pin = 3;arg4.pin = 4;arg5.pin = 5;arg6.pin = 6;
    xTaskCreate(vBlinkTask, "Blink Task", 128, (void *)&arg1, 1, NULL);
    xTaskCreate(vBlinkTask, "Blink Task2", 128, (void *)&arg2, 1, NULL);
-
-
+   xTaskCreate(vBlinkTask, "Blink Task3", 128, (void *)&arg3, 1, NULL);
+   xTaskCreate(vBlinkTask, "Blink Task4", 128, (void *)&arg4, 1, NULL);
+   xTaskCreate(vBlinkTask, "Blink Task5", 128, (void *)&arg5, 1, NULL);
+   xTaskCreate(vBlinkTask, "Blink Task6", 128, (void *)&arg6, 1, NULL);
    vTaskStartScheduler();
 }
 
