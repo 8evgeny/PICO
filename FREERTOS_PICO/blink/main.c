9c66447b6ff5a301c018e8e8940fd4fdbda108ce
8evgeny <8evgeny@gmail.com>
2023-11-03 23:15:54 +0300

    RTOS  работает но так параметры не передаются

--- FREERTOS_PICO/blink/main.c
+++ FREERTOS_PICO/blink/main.c
@@ -2,12 +2,12 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void vBlinkTask() {
+void vBlinkTask(uint8_t pin, uint16_t period) {
    for (;;) {
-      gpio_put(PICO_DEFAULT_LED_PIN, 1);
-      vTaskDelay(250);
-      gpio_put(PICO_DEFAULT_LED_PIN, 0);
-      vTaskDelay(250);
+      gpio_put(pin, 1);
+      vTaskDelay(period);
+      gpio_put(pin, 0);
+      vTaskDelay(period);
    }
 }
 
@@ -15,7 +15,20 @@ void main() {
 
    gpio_init(PICO_DEFAULT_LED_PIN);
    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-   xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
+   gpio_set_dir(4, GPIO_OUT);
+   typedef struct struct_arg_t {
+        uint8_t pin;
+        uint16_t period;
+   } arg;
+   arg arg1, arg2;
+   arg1.period = 100;
+   arg1.pin = PICO_DEFAULT_LED_PIN;
+   arg2.period = 300;
+   arg2.pin = 4;
+   xTaskCreate(vBlinkTask, "Blink Task", 128, (void*)&arg1, 1, NULL);
+   xTaskCreate(vBlinkTask, "Blink Task2", 128, (void*)&arg2, 1, NULL);
+
+
    vTaskStartScheduler();
 }
 
