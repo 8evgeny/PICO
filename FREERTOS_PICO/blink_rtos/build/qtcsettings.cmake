afcbe82f7ce7021fca7a1a8c19d1f08620ba2d5a
Evgeny <8evgeny@gmail.com>
2023-11-15 15:58:21 +0300

    REFACTORING

---
+++ FREERTOS_PICO/blink_rtos/build/qtcsettings.cmake
@@ -0,0 +1,9 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/evg/Qt6/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/arm-none-eabi-g++" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/arm-none-eabi-gcc" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "" CACHE "STRING" "" FORCE)
+set("PICO_SDK_PATH" "../pico-sdk" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "" CACHE "STRING" "" FORCE)
