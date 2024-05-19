3284aa999d0f28e70b8a5701b2a8fb263b029482
evgeny <8evgeny@gmail.com>
2024-05-19 08:37:18 +0300

    RGB

--- FREERTOS_PICO/blink/blink.c
+++ FREERTOS_PICO/blink/blink.c
@@ -10,13 +10,28 @@ int main() {
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
 #else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+//    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    const uint RED_PIN = 2;
+    const uint GREEN_PIN = 3;
+    const uint BLUE_PIN = 4;
+    gpio_init(RED_PIN);
+    gpio_init(GREEN_PIN);
+    gpio_init(BLUE_PIN);
+    gpio_set_dir(RED_PIN, GPIO_OUT);
+    gpio_set_dir(GREEN_PIN, GPIO_OUT);
+    gpio_set_dir(BLUE_PIN, GPIO_OUT);
     while (true) {
-        gpio_put(LED_PIN, 1);
+        gpio_put(RED_PIN, 1);
         sleep_ms(1000);
-        gpio_put(LED_PIN, 0);
+        gpio_put(RED_PIN, 0);
+        sleep_ms(1000);
+        gpio_put(GREEN_PIN, 1);
+        sleep_ms(1000);
+        gpio_put(GREEN_PIN, 0);
+        sleep_ms(1000);
+        gpio_put(BLUE_PIN, 1);
+        sleep_ms(1000);
+        gpio_put(BLUE_PIN, 0);
         sleep_ms(1000);
     }
 #endif
