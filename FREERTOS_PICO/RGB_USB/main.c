355191e119b707cf1d6e399114d1163a5657e747
evgeny <8evgeny@gmail.com>
2024-05-25 22:03:03 +0300

    USB RGB ++++

---
+++ FREERTOS_PICO/RGB_USB/main.c
@@ -0,0 +1,92 @@
+#include "pico/stdlib.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define CFG_TUSB_MCU OPT_MCU_RP2040
+#include "bsp/board.h"
+#include "tusb.h"
+const uint GREEN_PIN = 15;
+//------------- prototypes -------------//
+static void cdc_task(void);
+
+/*------------- MAIN -------------*/
+int main(void)
+{
+  board_init();
+  gpio_init(GREEN_PIN);
+  gpio_set_dir(GREEN_PIN, GPIO_OUT);
+  // init device stack on configured roothub port
+  tud_init(BOARD_TUD_RHPORT);
+
+  while (1)
+  {
+    tud_task(); // tinyusb device task
+    cdc_task();
+  }
+
+  return 0;
+}
+
+// echo to either Serial0 or Serial1
+// with Serial0 as all lower case, Serial1 as all upper case
+static void echo_serial_port(uint8_t itf, uint8_t buf[], uint32_t count)
+{
+  uint8_t const case_diff = 'a' - 'A';
+
+  for(uint32_t i=0; i<count; i++)
+  {
+    if (itf == 0)
+    {
+      // echo back 1st port as lower case
+      if (isupper(buf[i])) buf[i] += case_diff;
+    }
+    else
+    {
+      // echo back 2nd port as upper case
+      if (islower(buf[i])) buf[i] -= case_diff;
+    }
+
+    tud_cdc_n_write_char(itf, buf[i]);
+  }
+  tud_cdc_n_write_flush(itf);
+}
+
+//--------------------------------------------------------------------+
+// USB CDC
+//--------------------------------------------------------------------+
+static void cdc_task(void)
+{
+  uint8_t itf;
+
+  for (itf = 0; itf < CFG_TUD_CDC; itf++)
+  {
+    // connected() check for DTR bit
+    // Most but not all terminal client set this when making connection
+    // if ( tud_cdc_n_connected(itf) )
+    {
+      if ( tud_cdc_n_available(itf) )
+      {
+        uint8_t buf[64];
+
+        uint32_t count = tud_cdc_n_read(itf, buf, sizeof(buf));
+if (buf[0] == '1')
+{
+//    setup_gpio();
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
+    gpio_put(GREEN_PIN, 1);
+}
+if (buf[0] == '0')
+{
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+    gpio_put(GREEN_PIN, 0);
+}
+
+
+        // echo back to both serial ports
+//        echo_serial_port(0, buf, count);
+//        echo_serial_port(1, buf, count);
+      }
+    }
+  }
+}
--- FREERTOS_PICO/RGB_USB/rgb_usb.c
+++ FREERTOS_PICO/RGB_USB/rgb_usb.c
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include "pico/stdlib.h"
 #if 0
 - должно быть pico-sdk
@@ -11,9 +5,7 @@
 - перетащить файл uf2 в корень
 #endif
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
+
 //    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     const uint RED_PIN = 2;
     const uint GREEN_PIN = 3;
@@ -28,29 +20,29 @@ int main() {
     gpio_set_dir(RED_PIN, GPIO_OUT);
     gpio_set_dir(GREEN_PIN, GPIO_OUT);
     gpio_set_dir(BLUE_PIN, GPIO_OUT);
-    while (true) {
-        char ch = uart_getc(uart0);
-        if (ch == '1'){
-            gpio_put(RED_PIN, 1);
-            gpio_put(GREEN_PIN, 0);
-            gpio_put(BLUE_PIN, 0);
-        }
-        if (ch == '2'){
-            gpio_put(RED_PIN, 0);
-            gpio_put(GREEN_PIN, 1);
-            gpio_put(BLUE_PIN, 0);
-        }
-        if (ch == '3'){
-            gpio_put(RED_PIN, 0);
-            gpio_put(GREEN_PIN, 0);
-            gpio_put(BLUE_PIN, 1);
-        }
-        if (ch == '0'){
-            gpio_put(RED_PIN, 0);
-            gpio_put(GREEN_PIN, 0);
-            gpio_put(BLUE_PIN, 0);
-        }
-        uart_putc(uart0, ch);
-    }
-#endif
+//    while (true)
+//    {
+//        char ch = uart_getc(uart0);
+//        if (ch == '1'){
+//            gpio_put(RED_PIN, 1);
+//            gpio_put(GREEN_PIN, 0);
+//            gpio_put(BLUE_PIN, 0);
+//        }
+//        if (ch == '2'){
+//            gpio_put(RED_PIN, 0);
+//            gpio_put(GREEN_PIN, 1);
+//            gpio_put(BLUE_PIN, 0);
+//        }
+//        if (ch == '3'){
+//            gpio_put(RED_PIN, 0);
+//            gpio_put(GREEN_PIN, 0);
+//            gpio_put(BLUE_PIN, 1);
+//        }
+//        if (ch == '0'){
+//            gpio_put(RED_PIN, 0);
+//            gpio_put(GREEN_PIN, 0);
+//            gpio_put(BLUE_PIN, 0);
+//        }
+//        uart_putc(uart0, ch);
+//    }
 }
